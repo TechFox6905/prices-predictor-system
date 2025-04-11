11b1ae4f622ab04975ddf9d0afc0d2da88849414
Prathamesh <prathamesh.gharat07@gmail.com>
2025-04-11 20:51:05 +0530

    Feature Engineering done

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,5 +1,6 @@
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
+from steps.feature_engineering_step import feature_engineering_step
 from zenml import Model, pipeline, step
 
 
@@ -19,6 +20,11 @@ def ml_pipeline():
 
     # Handling Missing Values Step
     filled_data = handle_missing_values_step(raw_data)
+
+    # Feature Engineering Step
+    engineered_data = feature_engineering_step(
+        filled_data, strategy="log", features=["Gr Liv Area", "SalePrice"]
+    )
 
 if __name__ == "__main__":
     # Running the pipeline
