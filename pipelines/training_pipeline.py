2929f9add05b3f5c12f3434e15ffc10e0292a88e
Prathamesh <prathamesh.gharat07@gmail.com>
2025-04-12 13:56:56 +0530

    Data splitter added

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -2,6 +2,7 @@ from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
 from steps.feature_engineering_step import feature_engineering_step
 from steps.outlier_detection_step import outlier_detection_step
+from steps.data_splitter_step import data_splitter_step
 from zenml import Model, pipeline, step
 
 
@@ -29,6 +30,9 @@ def ml_pipeline():
 
     # Outlier Detection Step
     clean_data = outlier_detection_step(engineered_data, column_name="SalePrice")
+
+    # Data Splitting Step
+    X_train, X_test, y_train, y_test = data_splitter_step(clean_data, target_column="SalePrice")
 
 if __name__ == "__main__":
     # Running the pipeline
